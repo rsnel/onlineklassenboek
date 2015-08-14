6a611682773ba6a2bf687221b543d59bd48c0cbe
Rik Snel <rik@snel.it>
2015-08-14 17:08:10 +0200

    add ppl_id to output

--- sql/missing_access.php
+++ sql/missing_access.php
@@ -4,7 +4,7 @@
 include "../include/init.php";
 
 $result = mysql_query_safe(<<<EOQ
-SELECT DISTINCT login, email, altlogin, KB_NAAM(naam0, naam1, naam2) naam
+SELECT DISTINCT ppl_id, login, email, altlogin, KB_NAAM(naam0, naam1, naam2) naam
 FROM grp
 JOIN grp2vak USING (grp_id)
 JOIN doc2grp2vak USING (grp2vak_id)
@@ -16,7 +16,7 @@ EOQ
 );
 
 while ($row = mysql_fetch_assoc($result)) {
-	echo("{$row['login']} {$row['altlogin']} {$row['email']} {$row['naam']}\n");
+	echo("{$row['ppl_id']} {$row['login']} {$row['altlogin']} {$row['email']} {$row['naam']}\n");
 }
 
 ?>
